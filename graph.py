8e3f4f1bd95d4a50d1b7159e1e72c4effec4eb7d
einsjakob <90574451+einsjakob@users.noreply.github.com>
2024-02-14 23:04:50 +0100

    upgraded graph.py

--- graph.py
+++ graph.py
@@ -1,9 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xvalues = ["ja", "nein"]
-yvalues = [3,8]
-plt.barh(xvalues, yvalues)
-plt.xlabel("Anzahl der Stimmen")
-plt.ylabel("Antwortmöglichkeiten")
+fig = plt.figure()
+ax = plt.gca()
+
+font = {'family': 'serif',
+        'color':  'white',
+        'weight': 'normal',
+        'size': 12,
+        }
+
+fig.patch.set_facecolor("#49565c")
+ax.set_facecolor("#49565c")
+
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.spines["left"].set_color("#ffffff")
+ax.spines["bottom"].set_color("#ffffff")
+
+xvalueja = ["ja"]
+yvalueja = [3]
+xvaluenein = ["nein"]
+yvaluenein = [8]
+
+p1 = plt.barh(xvalueja, yvalueja, color="#00ff00")
+p2 = plt.barh(xvaluenein, yvaluenein, color="#ff0000")
+
+plt.xlabel("Anzahl der Stimmen", fontdict=font)
+plt.ylabel("Antwortmöglichkeiten", fontdict=font)
 plt.show()
