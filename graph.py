6131816ddd602408022a8b21f6629b50ee3e58e8
einsjakob <90574451+einsjakob@users.noreply.github.com>
2024-02-14 23:30:05 +0100

    upgrade graph.py

--- graph.py
+++ graph.py
@@ -1,31 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+#defining
 fig = plt.figure()
 ax = plt.gca()
 
+#font settings
 font = {'family': 'serif',
         'color':  'white',
         'weight': 'normal',
         'size': 12,
         }
 
+#set background color
 fig.patch.set_facecolor("#49565c")
 ax.set_facecolor("#49565c")
 
+#configure spines
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
 ax.spines["left"].set_color("#ffffff")
 ax.spines["bottom"].set_color("#ffffff")
 
+#setting tick color
+plt.tick_params(color="#ffffff",labelcolor="#ffffff")
+
+#setting values
 xvalueja = ["ja"]
 yvalueja = [3]
 xvaluenein = ["nein"]
 yvaluenein = [8]
 
+#configuring bars
 p1 = plt.barh(xvalueja, yvalueja, color="#00ff00")
 p2 = plt.barh(xvaluenein, yvaluenein, color="#ff0000")
 
+#configuring labels
 plt.xlabel("Anzahl der Stimmen", fontdict=font)
 plt.ylabel("Antwortmöglichkeiten", fontdict=font)
+
+#showing graph
 plt.show()
