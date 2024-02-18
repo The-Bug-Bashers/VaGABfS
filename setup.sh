9321e44862063926bf8bd67dc342d1296af6c432
dagadeta <treriwin@gmail.com>
2024-02-18 16:36:58 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+latestVersion=$(curl -s https://api.github.com/repos/AsamK/signal-cli/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"tag_name": "//;s/"//')
+echo "Latest signal-cli Version: $latestVersion"
+
+sudo apt update
+sudo apt install
+
+sudo apt install openjdk-18-jre
+
+wget https://github.com/AsamK/signal-cli/releases/download/$latestVersion/signal-cli-${latestVersion#v}.tar.gz
+sudo tar xf signal-cli-${latestVersion#v}.tar.gz -C /opt
+sudo ln -sf /opt/signal-cli-${latestVersion#v}/bin/signal-cli /usr/local/bin/
+
+sudo apt install screen
+
+git clone https://github.com/The-Bug-Bashers/VaGABfS.git
+wget https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png
+
+mv /root/VaGABfS/* /root/
