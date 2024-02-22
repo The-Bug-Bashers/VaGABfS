c831aeae0daf73718bcb813f34cc2e029709c0aa
einsjakob <90574451+einsjakob@users.noreply.github.com>
2024-02-22 14:30:21 +0100

    update setup.sh
    added python, added matplotlib, added finish message, changed mv path

--- setup.sh
+++ setup.sh
@@ -3,17 +3,20 @@ latestVersion=$(curl -s https://api.github.com/repos/AsamK/signal-cli/releases/l
 echo "Latest signal-cli Version: $latestVersion"
 
 sudo apt update
-sudo apt install
 
-sudo apt install openjdk-18-jre
+sudo apt install python-3 phython-matplotlib -y
+
+sudo apt install openjdk-18-jre -y
 
 wget https://github.com/AsamK/signal-cli/releases/download/$latestVersion/signal-cli-${latestVersion#v}.tar.gz
 sudo tar xf signal-cli-${latestVersion#v}.tar.gz -C /opt
 sudo ln -sf /opt/signal-cli-${latestVersion#v}/bin/signal-cli /usr/local/bin/
 
-sudo apt install screen
+sudo apt install screen -y
 
 git clone https://github.com/The-Bug-Bashers/VaGABfS.git
 wget https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png
 
-mv /root/VaGABfS/* /root/
+mv ~/VaGABfS/* ~/
+
+echo "Installation finished."
